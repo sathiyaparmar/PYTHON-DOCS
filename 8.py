0a6eae12417d2b0e038aec1242b8709e3a84f19c
sathiyaparmar <sathiyaparmar4999@gmail.com>
2022-02-28 17:44:48 +0530

    Loop

---
+++ 8.py
@@ -0,0 +1,26 @@
+"""
+loops - If dev wants to repeat some task
+loops types (Major)?
+	- Counter control - which dev can count
+	- Sentinal control - which dev can not count
+
+Minor types
+	- for
+	- while
+	- do.. while (not included in py)
+"""
+
+# myList = [1,2,3,4,]
+# print(myList)
+
+# for loop
+# for <some valriables> in <some iterables>:
+# 	<loop body>
+
+# for currentElement in myList:
+# 	print(currentElement)
+
+myList = ['Alice','Bob','Cairo','Denis']
+myScore = [20,56,78,9]
+
+# My score with Alice is 20 %

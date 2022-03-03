ef8b9aea5c1cf75e13b69ce3de86739ab2e71f15
sathiyaparmar <sathiyaparmar4999@gmail.com>
2022-03-03 18:16:38 +0530

    Range

---
+++ 8c.py
@@ -0,0 +1,17 @@
+# Range function
+
+# What is slice?
+myList = [1,2,3,4]
+x = myList[::2] # [startIndex:stopIndex:step]
+#print(x)
+
+# "built-in functions or methods generally returns a value"
+# a = sum(myList)
+# print(a)
+
+# "type" type objects generally returns descriptions of objects
+# a = type(myList)
+# print(a)
+
+# a = enumerate(myList)
+# print(list(a))
---
+++ 8d.py
@@ -0,0 +1,2 @@
+# Make list of odd numbers from 1 to 100
+# Make list of even numbers from 1 to 100

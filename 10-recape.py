82129d24b8ada4558e2bcfe58f2079f6806a6e6d
sathiyaparmar <sathiyaparmar4999@gmail.com>
2022-03-10 18:03:05 +0530

    Recape 1

---
+++ 10-recape.py
@@ -0,0 +1,36 @@
+"""
+- python?
+- python version?
+	- python --version
+- compiler?
+- interpreter?
+
+- variable?
+- data type?
+	- system defined
+		- bool, int, str, list, set ,...
+	- user defined
+		- class
+
+- operators
+	- arithmatic operators
+	- comparative operators
+	- boolean
+
+- del keyword deletes object from memory
+
+- list
+	- append
+	- insert
+	- remove
+	- pop
+	- extend
+	- + operator
+
+- loop
+	- counter
+	- sentinal
+
+	for <vars> in <iters>:
+		<body>
+"""

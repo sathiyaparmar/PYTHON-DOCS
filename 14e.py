a6b7a8baa9c0e6a8b52a50e7aeecad2400546cce
sathiyaparmar <sathiyaparmar4999@gmail.com>
2022-03-21 17:35:27 +0530

    Dictionary tutorials

---
+++ 14e.py
@@ -0,0 +1,26 @@
+"""
+Dictionary methods
+"""
+
+dOld = {}
+dOld['key1'] = 1
+dOld['key2'] = 2
+
+# Copy a dict
+# dNew = dOld.copy()
+# dNew['key3'] = 3
+
+# print(dOld)
+
+# update a dict
+# dSomeNew = {'key2':50}
+# dOld.update(dSomeNew)
+# print(dOld)
+
+# Delete a key
+# del dOld['key1']
+# print(dOld)
+
+# Check if a key exists
+# if 'key1' in dOld:
+# 	print('yes')
---
+++ 14f.py
@@ -0,0 +1,14 @@
+# database = {
+# 	"student_name":{
+# 		"science":59,
+# 		"maths":96,
+# 		"english":78
+# 	},
+# 	"stuent_name2":
+# 	{
+# 		""
+# 	}
+# }
+
+DATABASE = {}
+
